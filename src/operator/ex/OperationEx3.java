258ee92e58705858567fe0c14e3acfa8a27926d2
robb <mul501project@gmail.com>
2024-07-28 19:37:19 +0900

    add section3

---
+++ src/operator/ex/OperationEx3.java
@@ -0,0 +1,10 @@
+package operator.ex;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 100;
+        boolean result = 80 <= score && score <= 100;
+
+        System.out.println(result);
+    }
+}
